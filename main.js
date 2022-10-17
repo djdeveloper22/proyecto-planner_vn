d34db782b7211b08ed20e313b78bce3dff9344c1
pime panama <102924815+pimepan@users.noreply.github.com>
2022-10-17 15:28:39 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// //funcion carrito
+
+
+
+// let contadorCart = document.querySelector("#contadorCart");
+// let container_card = document.querySelector(".container-card");
+// let contenedor_card = document.querySelector(".container__card");
+// const carrito = document.querySelector("#carrito");
+// const contenedorProductos = document.querySelector("#contenedor__productos");
+// const cerrar = document.querySelector("#cerrar");
+// const btnCarrito = document.querySelector("#btnCarrito");
+// //const container__card = document.querySelector('.container__card');
+// let cont = 0;
+
+// carrito.addEventListener("click", () => {
+//   contenedorProductos.style.right = "0";
+// });
+
+// cerrar.addEventListener("click", () => {
+//   contenedorProductos.style.right = "-375px";
+// });
+
+// btnCarrito.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   cont++;
+//   contadorCart.textContent = +`${cont}`;
+//   alert("producto agregado");
+// });
+
+
+
+
